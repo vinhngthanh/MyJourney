07003040ba697605315916d0613f3cc0a0eedfb8
Vinh Nguyen <vinh28062002@gmail.com>
2023-02-01 20:33:44 -0500

    version 1 finish

--- app.js
+++ app.js
@@ -1,129 +1,130 @@
 if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
+  require("dotenv").config();
 }
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const session = require('express-session');
-const flash = require('connect-flash');
-const ExpressError = require('./utils/ExpressError');
-const methodOverride = require('method-override');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user');
-const helmet = require('helmet');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const helmet = require("helmet");
 
-const mongoSanitize = require('express-mongo-sanitize');
+const mongoSanitize = require("express-mongo-sanitize");
 
-const campgroundsRoutes = require('./routes/campgrounds');
-const reviewsRoutes = require('./routes/reviews');
-const usersRoutes = require('./routes/users');
+const campgroundsRoutes = require("./routes/campgrounds");
+const reviewsRoutes = require("./routes/reviews");
+const usersRoutes = require("./routes/users");
 
-const MongoDBStore = require('connect-mongo');
+const MongoDBStore = require("connect-mongo");
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 // const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 
 async function main() {
-    await mongoose.connect(dbUrl);
+  await mongoose.connect(dbUrl);
 }
 
 main()
-    .then(() => {
-        console.log("connection open!!");
-    })
-    .catch(err => console.log(err));
+  .then(() => {
+    console.log("connection open!!");
+  })
+  .catch((err) => console.log(err));
 
 const app = express();
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(mongoSanitize({
-    replaceWith: '_'
-}))
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
 
-const secret = process.env.SECRET || 'secret';
+const secret = process.env.SECRET || "secret";
 
 const store = MongoDBStore.create({
-    mongoUrl: dbUrl,
-    touchAfter: 24 * 60 * 60,
-    crypto: {
-        secret
-    }
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret,
+  },
 });
 
 store.on("error", function (e) {
-    console.log("SESSION ERROR", e);
-})
+  console.log("SESSION ERROR", e);
+});
 
 const sessionConfig = {
-    store,
-    name: 'session',
-    secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        // secure: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
+  store,
+  name: "session",
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    // secure: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 
 app.use(session(sessionConfig));
 app.use(flash());
 
 const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://api.mapbox.com/",
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
 ];
 const styleSrcUrls = [
-    "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net"
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net",
 ];
 const connectSrcUrls = [
-    "https://api.mapbox.com/",
-    "https://a.tiles.mapbox.com/",
-    "https://b.tiles.mapbox.com/",
-    "https://events.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
 ];
 const fontSrcUrls = [];
 app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: [],
-            connectSrc: ["'self'", ...connectSrcUrls],
-            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-            workerSrc: ["'self'", "blob:"],
-            objectSrc: [],
-            imgSrc: [
-                "'self'",
-                "blob:",
-                "data:",
-                "https://res.cloudinary.com/ntv2806/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-                "https://images.unsplash.com/",
-            ],
-            fontSrc: ["'self'", ...fontSrcUrls],
-        },
-    })
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", "blob:"],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        "blob:",
+        "data:",
+        "https://res.cloudinary.com/ntv2806/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+        "https://images.unsplash.com/",
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
 );
-
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -133,72 +134,72 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
-})
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 app.use("/", usersRoutes);
 app.use("/campgrounds", campgroundsRoutes);
 app.use("/campgrounds/:id/reviews", reviewsRoutes);
 
-app.get('/author', (req, res) => {
-    res.render('author/general');
-})
+app.get("/author", (req, res) => {
+  res.render("author/general");
+});
 
-app.get('/author/about/info', (req, res) => {
-    res.render('author/info');
-})
+app.get("/author/about/info", (req, res) => {
+  res.render("author/info");
+});
 
-app.get('/author/about/interest', (req, res) => {
-    res.render('author/interest');
-})
+app.get("/author/about/interest", (req, res) => {
+  res.render("author/interest");
+});
 
-app.get('/author/education/college', (req, res) => {
-    res.render('author/college');
-})
+app.get("/author/education/college", (req, res) => {
+  res.render("author/college");
+});
 
-app.get('/author/education/highschool', (req, res) => {
-    res.render('author/highschool');
-})
+app.get("/author/education/highschool", (req, res) => {
+  res.render("author/highschool");
+});
 
-app.get('/author/experience/viettel', (req, res) => {
-    res.render('author/viettel');
-})
+app.get("/author/experience/viettel", (req, res) => {
+  res.render("author/viettel");
+});
 
-app.get('/author/experience/university', (req, res) => {
-    res.render('author/uni');
-})
+app.get("/author/experience/university", (req, res) => {
+  res.render("author/uni");
+});
 
-app.get('/author/experience/eup', (req, res) => {
-    res.render('author/eup');
-})
+app.get("/author/experience/eup", (req, res) => {
+  res.render("author/eup");
+});
 
-app.get('/author/projects/numberpuzzle', (req, res) => {
-    res.render('author/numberpuzzle');
-})
+app.get("/author/projects/numberpuzzle", (req, res) => {
+  res.render("author/numberpuzzle");
+});
 
-app.get('/author/projects/stocktracker', (req, res) => {
-    res.render('author/stocktracker');
-})
+app.get("/author/projects/stocktracker", (req, res) => {
+  res.render("author/stocktracker");
+});
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.all('*', (req, res, next) => {
-    next(new ExpressError('PAGE NOT FOUND', 404));
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("PAGE NOT FOUND", 404));
+});
 
 app.use((err, req, res, next) => {
-    const { status = 500 } = err;
-    if (!err.message) err.message = 'Something went wrong';
-    res.status(status).render('error', { err });
-})
+  const { status = 500 } = err;
+  if (!err.message) err.message = "Something went wrong";
+  res.status(status).render("error", { err });
+});
 
 const port = process.env.PORT || 3000;
 // const port = 3000;
 app.listen(port, () => {
-    console.log(`serving on port ${port}`);
-})
+  console.log(`serving on port ${port}`);
+});
