9f50ed1d33e173ac4ed02e65511505027a4115cb
Vinh Nguyen <vinh28062002@gmail.com>
2023-09-11 16:50:06 -0400

    new map

---
+++ public/javascripts/hochiminhPageMap.js
@@ -0,0 +1,18 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10',
+    center: [106.660172, 10.762622],
+    zoom: 10
+});
+map.addControl(new mapboxgl.NavigationControl());
+
+new mapboxgl.Marker()
+    .setLngLat([106.660172, 10.762622])
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>My Hometown</h3><p>Ho Chi Minh City, Vietnam</p>`
+            )
+    )
+    .addTo(map)
